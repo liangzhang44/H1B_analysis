584efd74b86d2025ec0b5393c5be75fc6d4028b4
Liang Zhang <liazhang@uw.edu>
2018-10-29 23:31:54 -0700

    Updates

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#!/usr/bin/env python3
 #
 # Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python
 #
